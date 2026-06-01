9ac64ee06d9752b65b9e83bd76cfd68a114166c8
PantelisTs <pantsotsgr@gmail.com>
2026-06-01 03:37:23 +0300

    feat(auth): add LoginPage component

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,72 @@
+import {Field, FieldLabel} from "@/components/ui/field.tsx";
+import {Input} from "@/components/ui/input.tsx";
+import {Button} from "@/components/ui/button.tsx";
+import {useForm} from "react-hook-form";
+import {type LoginFields, loginSchema} from "@/schemas/auth.ts";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {useAuth} from "@/context/AuthProvider.tsx";
+import {toast} from "sonner";
+import {useNavigate} from "react-router";
+
+const LoginPage = () => {
+    const { loginUser } = useAuth();
+    const navigate = useNavigate();
+
+    const {
+        register,
+        handleSubmit,
+        formState: {errors, isSubmitting}
+    } = useForm<LoginFields>({
+        resolver: zodResolver(loginSchema)
+    });
+
+    const onSubmit = async (data: LoginFields): Promise<void> => {
+        try {
+            await loginUser(data);
+            toast.success("Login successful");
+            navigate("/products")
+
+        } catch (error) {
+            toast.error(error instanceof Error ? error.message : "Something went wrong. Login failed.");
+        }
+    }
+
+    return (
+        <>
+            <form
+                onSubmit={handleSubmit(onSubmit)}
+                className="max-w-sm mx-auto p-8 space-y-6 border rounded bg-white shadow-md shadow-cf-dark-red"
+            >
+                <h1 className="text-2xl font-bold text-center mb-4">Login</h1>
+                <Field>
+                    <FieldLabel htmlFor="username">Username</FieldLabel>
+                    <Input
+                        type="text"
+                        id="username"
+                        {...register("username")}
+                    />
+                    {errors.username && (
+                        <div className="text-cf-dark-red text-sm">{errors.username.message}</div>
+                    )}
+                </Field>
+                <Field>
+                    <FieldLabel htmlFor="password">Password</FieldLabel>
+                    <Input
+                        type="password"
+                        id="password"
+                        {...register("password")}
+                    />
+                    {errors.password && (
+                        <div className="text-cf-dark-red text-sm">{errors.password.message}</div>
+                    )}
+                </Field>
+
+                <Button type="submit" className="w-full">
+                    {isSubmitting ? "Logging in..." : "Login"}
+                </Button>
+            </form>
+        </>
+    )
+}
+
+export default LoginPage;
